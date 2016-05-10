6c88be15cfd4011d1f7d7010fc0a1022e10dc3f3
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-10 19:36:54 -0400

    bug of hackerRank
    HackerRank has a bug in this program, because reading input of string does not work on the first time, so in order to pass the test cases I needed to call the scanner second time so that I could finally read the input

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+        /* Declare second integer, double, and String variables. */
+        int intNum = scan.nextInt();
+        double doubleNum = scan.nextDouble();
+        String s1 = scan.nextLine();
+        String s2 = scan.nextLine();
+        /* Read and save an integer, double, and String to your variables.*/
+        //System.out.println(newString);
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+intNum);
+        System.out.println(d+doubleNum);
+        System.out.println(s.concat(s2));
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
