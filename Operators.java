9ecc83768146a85edeb0bea7217c31fe5c614333
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-11 19:28:12 -0400

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double tip = mealCost/100*tipPercent;
+        double tax = mealCost/100*taxPercent;
+        double total = mealCost+tip+tax;
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(total);
+      
+        // Print your result
+        System.out.printf("The total meal cost is %d dollars.",totalCost);
+    }
+}
