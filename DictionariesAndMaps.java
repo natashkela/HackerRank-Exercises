992e1b8eb9f5f4e221cb048dd0e1c2858b15a889
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-17 19:19:26 +0400

    Create DictionariesAndMaps.java

---
+++ DictionariesAndMaps.java
@@ -0,0 +1,28 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Integer> hashmap = new HashMap<String,Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            hashmap.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(hashmap.containsKey(s)){
+                System.out.printf("%s=%d\n",s,hashmap.get(s));
+            }
+            else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
