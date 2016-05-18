507950904f489be74a63235c967de54dd59e85a0
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-18 17:58:44 +0400

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int result = 1;
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner read = new Scanner(System.in);
+        int number = read.nextInt();
+        System.out.println(factorial(number));
+    }
+    
+    public static int factorial(int number){
+        if(number>=1){
+            result = number * factorial(number-1);
+        }
+        return result;
+    }
+}
