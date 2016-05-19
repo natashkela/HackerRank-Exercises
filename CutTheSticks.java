129f54f33bc7420ce9123ee33361551dc8f697f8
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:17:38 -0400

    Update CutTheSticks.java

--- CutTheSticks.java
+++ CutTheSticks.java
@@ -4,6 +4,10 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+Problem Description: https://www.hackerrank.com/challenges/cut-the-sticks
+*/
+
 public class Solution {
 
     public static void main(String[] args) {
