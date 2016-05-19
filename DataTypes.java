fe37650d89877b74249f12cbbab8d857cfd07dc3
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:18:09 -0400

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -4,6 +4,10 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+Problem Description: https://www.hackerrank.com/challenges/30-data-types
+*/
+
 public class Solution {
 	
     public static void main(String[] args) {
