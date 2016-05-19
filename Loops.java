dba1b18c376c4e728d7e3ea424045e590cb99518
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:23:46 -0400

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -3,7 +3,9 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
+/*
+Problem Description: https://www.hackerrank.com/contests/30-days-of-code/challenges/day-5-loops
+*/
 public class Solution {
 
     public static void main(String[] args) {
