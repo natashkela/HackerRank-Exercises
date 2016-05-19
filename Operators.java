81af9eadeed63435341d907549e706f5b84e64ee
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:24:17 -0400

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -3,6 +3,9 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+/*
+Problem Description: https://www.hackerrank.com/challenges/30-operators
+*/
 public class Arithmetic {
 
     public static void main(String[] args) {
