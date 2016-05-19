39e70140ec22419c837088f3ccd5c41b28a1c7fe
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:16:11 -0400

    Update ChocolateFeast.java

--- ChocolateFeast.java
+++ ChocolateFeast.java
@@ -4,6 +4,10 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+/*
+Problem Description: https://www.hackerrank.com/challenges/chocolate-feast
+*/
 public class Solution {
 
     public static void main(String[] args) {
