c383ae81ecaa451159d0add8b7be605699b25bab
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:18:53 -0400

    Update DiagonalDifference.java

--- DiagonalDifference.java
+++ DiagonalDifference.java
@@ -4,6 +4,10 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+Problem Description: https://www.hackerrank.com/challenges/diagonal-difference
+*/
+
 public class Solution {
 
     public static void main(String[] args) {
