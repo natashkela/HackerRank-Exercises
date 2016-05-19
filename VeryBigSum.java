298cc64144e17ebafbd88ba2ec32624c6c926029
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-19 10:30:43 -0400

    Update VeryBigSum.java

--- VeryBigSum.java
+++ VeryBigSum.java
@@ -4,6 +4,10 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+Problem Description: https://www.hackerrank.com/challenges/a-very-big-sum
+*/
+
 public class Solution {
 
     public static void main(String[] args) {
