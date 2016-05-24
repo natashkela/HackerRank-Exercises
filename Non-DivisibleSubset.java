29efe8bbbd4c3f5f2f89c84ceb8235f58c774529
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-23 21:25:55 -0400

    passes little more test cases
    the way of thinking about this algorithm is wrong

--- Non-DivisibleSubset.java
+++ Non-DivisibleSubset.java
@@ -32,6 +32,15 @@ public class Solution {
                 }
             }
         }
+        
+        
+        for(int i=0;i<list.size()-1;i++){
+            for(int j=i+1;j<list.size();j++){
+                if((list.get(i)+list.get(j))%k==0){
+                    list.remove(i);
+                }
+            }
+        }
         System.out.println(list.size());
     }
 }
