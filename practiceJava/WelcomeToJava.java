8ccb1fcdd470671ac613229cafeaaed41545916c
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-29 19:12:04 -0400

    Create WelcomeToJava.java

---
+++ practiceJava/WelcomeToJava.java
@@ -0,0 +1,14 @@
+public class Solution {
+   
+   /*
+   Problem Description: https://www.hackerrank.com/challenges/welcome-to-java
+   */
+   
+   public static void main(String []argv)
+   {
+      //Write your solution here.
+       System.out.println("Hello World.");
+       System.out.println("Hello Java.");
+   }
+
+}
