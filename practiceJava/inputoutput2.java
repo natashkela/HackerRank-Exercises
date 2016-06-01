11e504085a6e69e2b9b21f9ff005bb20b115ebf5
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-05-31 22:29:27 -0400

    Create inputoutput2.java

---
+++ practiceJava/inputoutput2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+/*
+Problem Description:  https://www.hackerrank.com/challenges/java-stdin-stdout
+*/
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            double y = sc.nextDouble();
+        sc.nextLine();
+            String s = sc.nextLine();          //Complete this code
+
+            System.out.println("String: "+s);
+            System.out.println("Double: "+y);
+            System.out.println("Int: "+x);
+    }
+}
