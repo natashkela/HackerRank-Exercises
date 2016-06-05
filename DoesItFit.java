0577f9f8d7d7c1aad132db88e73a456695eca6e7
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-06-05 00:40:56 -0400

    Passes two test cases times out on other ones

--- DoesItFit.java
+++ DoesItFit.java
@@ -4,50 +4,50 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-/*
-Problem Description: https://www.hackerrank.com/contests/zalando-codesprint/challenges/does-it-fit
-*/
-
 public class Solution {
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int width = read.nextInt();
-        int height = read.nextInt();
-        int area = width*height;
-        int num = read.nextInt();
-        read.nextLine();
-        for(int i=0;i<num;i++){
-            String line = read.nextLine();
-            //double ShapeArea =0;
-            int wid=0;
-            int hei=0;
-            int r=0;
-            char shape = line.charAt(0);
-            if(shape == 'R'){
-             wid = Character.getNumericValue(line.charAt(2));
-             hei = Character.getNumericValue(line.charAt(4));
+        int n = read.nextInt();
+        int m = read.nextInt();//number of distict shoe ids
+        int k = read.nextInt();
+        LinkedList<Integer> list1 = new LinkedList<Integer>();
+        for(int i=0;i<k;i++){
+            int id = read.nextInt();
+            list1.add(id);
+        }
+        Integer number = list1.get(0);
+        int count=0;
+        LinkedList<Integer> countList = new LinkedList<Integer>();
+        LinkedList <Integer> numberList = new LinkedList<Integer>();
+        while(!list1.isEmpty()){
+            if(list1.contains(number)){
+                count++;
+                list1.remove(number);
             }
             else{
-                r = Character.getNumericValue(line.charAt(2));
-            }
-            if(shape=='C'){
-               if(2*r<=width&&2*r<=height){
-                   System.out.println("YES");
-               }
-               else{
-                   System.out.println("NO");
-               }
-            }
-            else {
-                if((wid<=width&&hei<=height)||(wid<=height&&hei<=width)){
-                    System.out.println("YES");
-                }
-                else{
-                    System.out.println("NO");
-                }
+                //System.out.println(number);
+                countList.add(count);
+                count=0;
+                numberList.add(number);
+                number=list1.get(0);
             }
         }
-        
+        countList.add(count);
+        numberList.add(number);
+        int z = 0;
+        while(z<n){
+            int max=countList.get(0); 
+            for(int i=0;i<countList.size();i++){
+                if(max<countList.get(i)){
+                    max = countList.get(i);
+                }
+            }
+            int index = countList.indexOf(max);
+            countList.remove(index);
+            System.out.println(numberList.get(index));
+            numberList.remove(index);
+            z++;
+        }
     }
 }
