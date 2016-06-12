7768a7ec78c0a8e89d6aa8c0369857542493a765
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-06-11 20:43:39 -0400

    Update inputoutput.java

--- practiceJava/inputoutput.java
+++ practiceJava/inputoutput.java
@@ -5,7 +5,6 @@ Problem Description: https://www.hackerrank.com/challenges/java-stdin-and-stdout
 */
 
 public class Solution {
-
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
       int a=sc.nextInt();
