5b4497e4add222d8594cf794e0fad7046454c406
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-06-19 19:52:31 -0400

    Create inputoutput.cpp

---
+++ cplusplus/inputoutput.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+Problem Description: https://www.hackerrank.com/challenges/cpp-input-and-output
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+    return 0;
+}
