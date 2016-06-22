82e3d53a5a0ea40d1dbb0aed358ea13f1eac40df
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-06-21 23:01:48 -0400

    Create basicDataTypes.cpp

---
+++ cplusplus/basicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Problem Description: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types
+*/
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+    cin>>a>>b>>c>>d>>e>>f;
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf",a,b,c,d,e,f);
+    return 0;
+}
