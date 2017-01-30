1b629f58ea2277913c986da2c08883f7248b1088
Nata Vacheishvili <nvacheishvili@wpi.edu>
2017-01-30 17:17:10 -0500

    Update MetroCard.java

--- codefights/MetroCard.java
+++ codefights/MetroCard.java
@@ -3,12 +3,21 @@ int[] metroCard(int lastNumberOfDays) {
     if(lastNumberOfDays==30){
         array = new int[1];
         array[0]=31;
+        return array;
     }
-    else{
+    else if(lastNumberOfDays==28){
+        array = new int[1];
+        array[0]=31;
+        return array;
+    }
+    else if(lastNumberOfDays==31){
         array=new int[3];
         array[0]=28;
         array[1]=30;
         array[2]=31;
+return array;
     }
-    return array;
+    
+    int[] a = new int[1];
+    return a;
 }
