5a68f749ab61987183eed950dbe145ca4e579651
Nata Vacheishvili <nvacheishvili@wpi.edu>
2017-02-10 18:49:57 -0500

    Update isCaseSensitivePalindrome.java

--- codefights/isCaseSensitivePalindrome.java
+++ codefights/isCaseSensitivePalindrome.java
@@ -1,14 +1,9 @@
 boolean isCaseInsensitivePalindrome(String inputString) {
     inputString = inputString.toLowerCase();
-    String compare="";
-    for(int i=inputString.length()-1;i>inputString.length()/2;i--){
-        compare+=inputString.charAt(i);
+    for(int i=0;i<inputString.length();i++){
+        if(inputString.charAt(i)!=inputString.charAt(inputString.length()-i-1)){
+            return false;
+        }
     }
-    System.out.println(compare);
-    if(inputString.substring(0,inputString.length()/2).equals(compare)){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return true;
 }
