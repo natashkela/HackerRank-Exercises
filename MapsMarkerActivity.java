4756ed3c5b8523654c125b65ff6be428a5cb9e6a
Nata Vacheishvili <nvacheishvili@wpi.edu>
2017-02-21 18:26:39 -0500

    Create MapsMarkerActivity.java

---
+++ MapsMarkerActivity.java
@@ -0,0 +1,131 @@
+ //-------------------SALISBURY----------------------------
+        LatLng salisbury = new LatLng(42.274610,-71.807064);
+        googleMap.addMarker(new MarkerOptions().position(salisbury).
+                title("Marker at Salisvbury Labs"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(salisbury));
+
+        //-------------------CC----------------------------
+        LatLng campus_center = new LatLng(42.274868,-71.808266);
+        googleMap.addMarker(new MarkerOptions().position(campus_center).
+                title("Marker at Campus Center"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(campus_center));
+
+        //-------------------Goddard----------------------------
+        LatLng goddard = new LatLng(42.274868,-71.808266);
+        googleMap.addMarker(new MarkerOptions().position(goddard).
+                title("Marker at Goddard Hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(goddard));
+
+        //-------------------AK----------------------------
+        LatLng ak = new LatLng(42.275321,-71.807134);
+        googleMap.addMarker(new MarkerOptions().position(ak).
+                title("Marker at Atwater kent"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ak));
+
+        //-------------------fulelr----------------------------
+        LatLng fuller = new LatLng(42.274976,-71.806405);
+        googleMap.addMarker(new MarkerOptions().position(fuller).
+                title("Marker at Fuller Hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fuller));
+
+        //-------------------kaven----------------------------
+        LatLng kaven = new LatLng(42.274837,-71.805831);
+        googleMap.addMarker(new MarkerOptions().position(kaven).
+                title("Marker at kaven Hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kaven));
+
+        //-------------------Library----------------------------
+        LatLng library = new LatLng(42.274249,-71.806431);
+        googleMap.addMarker(new MarkerOptions().position(library).
+                title("Marker at Gordgon Library"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(library));
+
+        //-------------------Boynton----------------------------
+        LatLng boynton = new LatLng(42.273467,-71.807236);
+        googleMap.addMarker(new MarkerOptions().position(boynton).
+                title("Marker at Boynton Hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(boynton));
+
+        //-------------------Washburn----------------------------
+        LatLng washburn = new LatLng(42.274082,-71.807193);
+        googleMap.addMarker(new MarkerOptions().position(washburn).
+                title("Marker at Washburn Laboratories"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(washburn));
+
+        //-------------------Olin----------------------------
+        LatLng olin = new LatLng(42.274960,-71.807955);
+        googleMap.addMarker(new MarkerOptions().position(olin).
+                title("Marker at Olin hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(olin));
+
+        //-------------------founders----------------------------
+        LatLng founders = new LatLng(42.273042,-71.805300);
+        googleMap.addMarker(new MarkerOptions().position(founders).
+                title("Marker at Founders hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(founders));
+
+        //-------------------faraday----------------------------
+        LatLng faraday = new LatLng(42.275011,-71.801577);
+        googleMap.addMarker(new MarkerOptions().position(faraday).
+                title("Marker at Founders hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(faraday));
+
+        //-------------------gateway----------------------------
+        LatLng gateway = new LatLng(42.275305,-71.799157);
+        googleMap.addMarker(new MarkerOptions().position(gateway).
+                title("Marker at gateway hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(gateway));
+
+        //-------------------institute----------------------------
+        LatLng institute = new LatLng(42.272288,-71.805573);
+        googleMap.addMarker(new MarkerOptions().position(institute).
+                title("Marker at institute hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(institute));
+
+        //-------------------Alden----------------------------
+        LatLng alden = new LatLng(42.273126,-71.808422);
+        googleMap.addMarker(new MarkerOptions().position(alden).
+                title("Marker at Alden hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(alden));
+
+        //-------------------Admissions center----------------------------
+        LatLng ac = new LatLng(42.273654,-71.808958);
+        googleMap.addMarker(new MarkerOptions().position(ac).
+                title("Marker at Admission Center"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ac));
+
+        //-------------------Fuller----------------------------
+        LatLng fuller_apartments = new LatLng(42.273126,-71.808422);
+        googleMap.addMarker(new MarkerOptions().position(fuller_apartments).
+                title("Marker at Fuller Apartmnets"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fuller_apartments));
+
+        //-------------------Daniels Hall----------------------------
+        LatLng daniels = new LatLng(42.273245,-71.810117);
+        googleMap.addMarker(new MarkerOptions().position(daniels).
+                title("Marker at daniels hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(daniels));
+
+        //-------------------Morgan Hall----------------------------
+        LatLng morgan = new LatLng(42.273408,-71.810895);
+        googleMap.addMarker(new MarkerOptions().position(morgan).
+                title("Marker at Morgan hall"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(morgan));
+
+        //-------------------The Quad----------------------------
+        LatLng quad = new LatLng(42.273820,-71.809757);
+        googleMap.addMarker(new MarkerOptions().position(quad).
+                title("Marker at The Quad"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(quad));
+
+        //-------------------The Gym----------------------------
+        LatLng gym = new LatLng(42.274182,-71.810524);
+        googleMap.addMarker(new MarkerOptions().position(gym).
+                title("Marker at The gym"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(gym));
+
+        //-------------------The Harrington Auditorium----------------------------
+        LatLng harington = new LatLng(42.274372,-71.809398);
+        googleMap.addMarker(new MarkerOptions().position(harington).
+                title("Marker at The harington"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(harington));
